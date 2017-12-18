2270f374584a0f4a604c5c2b9b4774e313ea6037
Fdslk <907081174@qq.com>
2017-12-18 09:59:01 +0800

    添加计算每个词条在文章中出现的次数的方法

--- py_code/text_classification/computer_word_vector.py
+++ py_code/text_classification/computer_word_vector.py
@@ -1,4 +1,9 @@
+# -*- coding: utf-8 -*-
 import numpy as np
+import os
+import sys
+from collections import defaultdict
+from collections import Counter
 
 class select_feature():
 	
@@ -12,19 +17,34 @@ class select_feature():
 		return docnum
 		
 	def count_Words(self, input_file):
-		data = []
+		data = {}
 		with open(input_file, 'r') as file:
 			for line in file.readlines():
-				row_list = []
-				row_list.append(line.split(' ')[0])
+				data[line.split(' ')[0]] = {}
 				word_num = (len(line.split(' ')) - 1)
-				row_list.append(str(word_num))
-				if row_list:
-					data.append(row_list)
+				data[line.split(' ')[0]] = word_num
 		return data
-	#s = b'\xe8\xae\xa1\xe7\xae\x97\xe6\x9c\xba\xe8\xaf\xbe'
-	#print(s.decode('utf-8'))			
-	# def count_term_frequency(self):
+		
+	def count_term_num(self, input_file):
+		# type: (object) -> object
+		data = {}
+		count = 0
+		with open(input_file, 'r') as file:
+			for line in file.readlines():
+				data[line.split(' ')[0]] = {}
+				for row_index in range(1, len(line.split(' '))):
+					if line.split(' ')[row_index] not in data[line.split(' ')[0]]:
+						data[line.split(' ')[0]][line.split(' ')[row_index]] = 1
+					else:
+						data[line.split(' ')[0]][line.split(' ')[row_index]] += 1
+				count += 1
+					#head, words = line.split(' ', 1)
+					#data[head] = {}
+					#data[head] =  dict(Counter(words.split(' ')))
+					#count += 1
+		print(count)
+		return data
+					
+	# def count_term_frequency(self, ):
 	# def compute_Words_IDF(self):
 	# def compute_word_tf_idf(self):
-		
